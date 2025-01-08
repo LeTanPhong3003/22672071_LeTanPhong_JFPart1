3334482e3451fab4287e34b9972457ababfbca3b
H82M19\Student <Student>
2025-01-08 10:29:09 +0700

    Finish JF part 1 #3

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-const markWeight1 = 78;
-const markHeight1 = 1.69;
-const johnWeight1 = 92;
-const johnHeight1 = 1.95;
-
-const markWeight2 = 95;
-const markHeight2 = 1.88;
-const johnWeight2 = 85;
-const johnHeight2 = 1.76;
-
-function calculateBMI(weight, height) {
-  return weight / (height * height);
+function calculateAverage(score1, score2, score3) {
+    return (score1 + score2 + score3) / 3;
 }
-
-const markBMI1 = calculateBMI(markWeight1, markHeight1);
-const johnBMI1 = calculateBMI(johnWeight1, johnHeight1);
-
-const markBMI2 = calculateBMI(markWeight2, markHeight2);
-const johnBMI2 = calculateBMI(johnWeight2, johnHeight2);
-
-function compareBMI(markBMI, johnBMI, dataset) {
-    if (markBMI > johnBMI) {
-      console.log(`${dataset}: Mark's BMI (${markBMI.toFixed(1)}) is higher than John's (${johnBMI.toFixed(1)})!`);
-    } else {
-      console.log(`${dataset}: John's BMI (${johnBMI.toFixed(1)}) is higher than Mark's (${markBMI.toFixed(1)})!`);
-    }
-  }
   
-  compareBMI(markBMI1, johnBMI1, "Data 1");
-  compareBMI(markBMI2, johnBMI2, "Data 2");
+function determineWinner(dolphinsScores, koalasScores) {
+    const dolphinsAvg = calculateAverage(...dolphinsScores);
+    const koalasAvg = calculateAverage(...koalasScores);
+  
+    console.log(`Dolphin\'s average score: ${dolphinsAvg}`);
+    console.log(`Koala\'s average score: ${koalasAvg}`);
+  
+    if (dolphinsAvg >= 100 && koalasAvg >= 100) {
+      if (dolphinsAvg > koalasAvg) {
+        console.log('Dolphins win the trophy!');
+      } else if (koalasAvg > dolphinsAvg) {
+        console.log('Koalas win the trophy!');
+      } else {
+        console.log('It\'s a draw!');
+      }
+    } else if (dolphinsAvg >= 100 && koalasAvg < 100) {
+      console.log('Dolphins win the trophy!');
+    } else if (koalasAvg >= 100 && dolphinsAvg < 100) {
+      console.log('Koalas win the trophy!');
+    } else {
+      console.log('No team wins because both teams has a score least than 100!');
+    }
+}
+  
+console.log("Test Data 1:");
+determineWinner([96, 108, 89], [88, 91, 110]);
+  
+console.log("\nTest Data Bonus 1:");
+determineWinner([97, 112, 101], [109, 95, 123]);
+  
+console.log("\nTest Data Bonus 2:");
+determineWinner([97, 112, 101], [109, 95, 106]);
   
